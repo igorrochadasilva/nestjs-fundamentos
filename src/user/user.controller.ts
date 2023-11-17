04176ccbe76f1ef68ffddf63c9b4e584e4016d1a
igoresilva <igor082011@gmail.com>
2023-11-17 12:59:45 -0300

    get method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,39 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Put } from '@nestjs/common';
 
 @Controller('users')
 export class UserController {
   @Post()
   async create(@Body() body) {
     return { body };
+  }
+
+  @Get()
+  async list() {
+    return {
+      users: [
+        {
+          name: 'user1',
+          email: 'user1@gmail.com',
+        },
+        {
+          name: 'user2',
+          email: 'user2@gmail.com',
+        },
+        {
+          name: 'user3',
+          email: 'user3@gmail.com',
+        },
+        ,
+        {
+          name: 'user4',
+          email: 'user4@gmail.com',
+        },
+      ],
+    };
+  }
+
+  @Get(':id')
+  async show(@Param() param) {
+    return { user: {}, param };
   }
 }
