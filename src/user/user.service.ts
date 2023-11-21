2ad26f294eb0543b2febe03589161bb8b6ce38ce
igoresilva <igor082011@gmail.com>
2023-11-21 18:25:40 -0300

    integration delete method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UpdatePutUserDTO } from './dto/update-put-user.dto';
@@ -34,6 +34,8 @@ export class UserService {
     id: number,
     { birthAt, name, password, email }: UpdatePutUserDTO,
   ) {
+    await this.exists(id);
+
     if (!birthAt) {
       birthAt = null;
     }
@@ -54,6 +56,8 @@ export class UserService {
     id: number,
     { birthAt, name, password, email }: UpdatePatchUserDTO,
   ) {
+    await this.exists(id);
+
     const data: any = {};
 
     if (birthAt) {
@@ -75,5 +79,21 @@ export class UserService {
         id,
       },
     });
+  }
+
+  async delete(id: number) {
+    await this.exists(id);
+
+    return this.prisma.user.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async exists(id: number) {
+    if (!(await this.show(id))) {
+      throw new NotFoundException(`O usuário ${id} não existe.`);
+    }
   }
 }
