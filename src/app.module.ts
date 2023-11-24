ba42217015363bc68d3803ef3c952c2e02aac544
igoresilva <igor082011@gmail.com>
2023-11-24 08:44:48 -0300

    assinar token jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { AuthMmodule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AuthMmodule],
   controllers: [AppController],
   providers: [AppService],
 })
